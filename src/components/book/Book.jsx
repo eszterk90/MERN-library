ede91c4dfce92df069d4f60a19ed39054356b231
eszterk90git config --global user.email eszter.kadar1@gmail.com <eszter.kadar1@gmail.com>
2022-08-25 15:12:22 +0200

    single book page styling

--- src/components/book/Book.jsx
+++ src/components/book/Book.jsx
@@ -1,4 +1,4 @@
-import React, {useContext, useState} from 'react'
+import React, {useContext, useEffect, useState} from 'react'
 import bookContext from '../../context/bookContext'
 import userContext from '../../context/userContext'
 import {useParams, useNavigate} from 'react-router-dom'
@@ -13,19 +13,24 @@ const {backToHome} = useContext(userContext);
 
 const book = books.find(b => b._id == bookId);
 
-
 const addToProfile = () => {
   addBookItem(book)
 }
 
   return (
-    <div className='single-book-container'>
-        <h3>{book.title[0].toUpperCase()+ book.title.slice(1)}</h3>
-        <h4>{book.author}</h4>
-        <img src={book.imageUrl}/>
-        <p>pages: {book.pages}</p>
-        {!book.rented_by ? <div><span onClick={addToProfile}>Add to cart</span></div> : <span>Not available</span>}
-        <button onClick={backToHome}>Go Home</button>
+    <div className='profile-container'>
+      <div className='books'>
+        <div className='book'>
+          {!book.rented_by ? <button className='card-btn checkin' onClick={addToProfile}>Add to cart</button> : <span>Not available</span>}
+          <button onClick={backToHome}>Go Home</button>
+          <img src={book.imageUrl}/>
+        </div>
+        <div className='book-info'>
+          <h3>{book.title[0].toUpperCase()+ book.title.slice(1)}</h3>
+          <h4>{book.author}</h4>
+          <p>pages: {book.pages}</p>
+        </div>
+      </div>
     </div>
   )
 }
