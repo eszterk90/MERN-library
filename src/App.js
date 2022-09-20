4ecd7e49b5b645f1407fc0a8eec3a0af96cae3e6
eszterk90git config --global user.email eszter.kadar1@gmail.com <eszter.kadar1@gmail.com>
2022-09-20 23:47:48 +0200

    modal created and styled beautifully

--- src/App.js
+++ src/App.js
@@ -5,13 +5,16 @@ import Authentication from './components/authentication/Authentication';
 import Book from './components/book/Book'
 import Profile from './components/profile/Profile'
 import UserContext from './context/userContext';
+import BookContext from './context/bookContext';
 import NavBar from './components/navbar/NavBar';
 import Verify from './components/authentication/Verify'
+import Modal from './components/book/Modal'
 
 
 
 function App() {
   const { currentUser } = useContext(UserContext);
+  const {currentBook} = useContext(BookContext);
  
 
   return (
@@ -23,12 +26,13 @@ function App() {
         : (
         <Route path='/' element={<Authentication />} />
         )}
-        <Route path='/:bookId' element={<Book />} /> 
+        {/*<Route path='/:bookId' element={<Book />} /> */}
         <Route path='/profile' element={<Profile />} />
         <Route path='/verify?authId=authId&secretKey=secretKey' element={<Verify/>} />
         {/*<Route path='*' element={<Navigate to='/' />} />*/}
       </Routes>
       {currentUser && <NavBar/>}
+      {currentBook && <Modal/>}
     </div>
   );
 }
