82b0c26c978cae8167fa2ac3cd13b95f6c359d25
eszterk90git config --global user.email eszter.kadar1@gmail.com <eszter.kadar1@gmail.com>
2022-09-28 09:49:11 +0200

    vercel baseurl

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
-// const baseUrl = 'https://library-backend.vercel.app/'
+const baseUrl = 'https://library-backend.vercel.app/'
 
-const baseUrl = 'https://mock-library.herokuapp.com/'
+// const baseUrl = 'https://mock-library.herokuapp.com/'
 
 // const baseUrl = 'http://localhost:5001/'
 
