f65aa9cdde6d953870299348322cef97514d96ec
eszterk90git config --global user.email eszter.kadar1@gmail.com <eszter.kadar1@gmail.com>
2022-10-01 12:42:06 +0200

    new backend deploy

--- src/config.js
+++ src/config.js
@@ -1,6 +1,8 @@
 // const baseUrl = 'https://library-backend-oovrpsq6n-eszterk90.vercel.app/'
 
-const baseUrl = 'https://mock-library.herokuapp.com/'
+// const baseUrl = 'https://mock-library.herokuapp.com/'
+
+const baseUrl = 'https://library-backend-new.herokuapp.com/'
 
 // const baseUrl = 'http://localhost:5001/'
 
